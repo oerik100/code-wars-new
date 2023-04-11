625f07e7888038c402c0643d5f290ff2ada537bd
Erik Olson <oerik100@gmail.com>
2023-04-11 07:31:42 -0400

    daily code wars 4/11/23

---
+++ 8kyu/RemoveDuplicatesFromList.js
@@ -0,0 +1,10 @@
+// Problems
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Solution
+function distinct(a) {
+    let unique = [...new Set(a)];
+    return unique;
+  }
