dfaa3a2311ab715daaefbbafa92935de88f145f8
Erik Olson <oerik100@gmail.com>
2023-05-02 07:13:10 -0400

    daily code war 5/2/23

---
+++ 8kyu/ReturnToSanity.js
@@ -0,0 +1,9 @@
+// Problems
+// This function should return an object, but it's not doing what's intended. What's wrong?
+
+// Solution
+function mystery() {
+  var results =
+    {sanity: 'Hello'};
+  return results;
+}
