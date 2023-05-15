5e77529140807f5dc150defd32a14ca60ec3df36
Erik Olson <oerik100@gmail.com>
2023-05-15 07:11:38 -0400

    daily code war 5/15/23

---
+++ 8kyu/CountNumberToReversedArrayOfDigits.js
@@ -0,0 +1,11 @@
+// Problems
+// Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+// Example(Input => Output):
+// 35231 => [1,3,2,5,3]
+// 0 => [0]
+
+// Solution
+function digitize(n) {
+    return n.toString().split("").reverse().map(Number);
+  }
