e8d3a21541959331e96837ae343cd9a7e5ff3aa3
Erik Olson <oerik100@gmail.com>
2023-06-22 07:05:52 -0400

    daily code war 6/22/23

---
+++ 8kyu/TripleTrouble.js
@@ -0,0 +1,9 @@
+// Problems
+// Create a function that will return a string that combines all of the letters of the three inputed strings in groups. Taking the first letter of all of the inputs and grouping them next to each other. Do this for every letter, see example below!
+
+// E.g. Input: "aa", "bb" , "cc" => Output: "abcabc"
+
+// Solution
+function tripleTrouble(one, two, three){
+    return one.split('').reduce((a,s,c)=>`${a}${one[c]}${two[c]}${three[c]}`,'')
+ }
