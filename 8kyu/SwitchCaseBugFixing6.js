2478a31334572ab930f32ec5ecb60fba8fbd7b4d
Erik Olson <oerik100@gmail.com>
2023-06-24 10:09:14 -0400

    daily code war 6/24/23

---
+++ 8kyu/SwitchCaseBugFixing6.js
@@ -0,0 +1,14 @@
+// Problems
+// Oh no! Timmy's evalObject function doesn't work. He uses Switch/Cases to evaluate the given properties of an object, can you fix timmy's function?
+
+// Solution
+function evalObject(value){
+    switch(value.operation){
+      case'+': return value.a + value.b;
+      case'-': return value.a - value.b;
+      case'/': return value.a / value.b;
+      case'*': return value.a * value.b;
+      case'%': return value.a % value.b;
+      case'^': return Math.pow(value.a, value.b);
+    }
+  }
