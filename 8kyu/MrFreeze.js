2b2f8ad2c7a255013c345664092fc3cfa62880f4
Erik Olson <oerik100@gmail.com>
2023-06-29 06:56:44 -0400

    daily code war 6/29/23

---
+++ 8kyu/MrFreeze.js
@@ -0,0 +1,5 @@
+// Problems
+// There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+
+// Solution
+Object.freeze(MrFreeze)
